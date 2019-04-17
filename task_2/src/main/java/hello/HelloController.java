9325b2bb879001db08e068da0a775add3a0fe911
olexiy2k <47949359+olexiy2k@users.noreply.github.com>
2019-04-17 20:07:40 +0300

    2

--- task_2/src/main/java/hello/HelloController.java
+++ task_2/src/main/java/hello/HelloController.java
@@ -1,32 +1,24 @@
 package hello;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import java.util.Map;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@AutoConfigureMockMvc
-public class HelloControllerTest {
+@RestController
+public class HelloController {
 
-  @Autowired
-  private MockMvc mvc;
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
 
-  @Test
-  public void getHello() throws Exception {
-    mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(content().string(equalTo("Greetings from Spring Boot!")));
-    
-  }
+    @GetMapping("/index")
+    public String Name(@RequestParam String name) {
+        return "Hello  " + name;
+    }
 }
